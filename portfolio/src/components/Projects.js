12d6364b13014fce9d8edd900975b93deb8445df
swagatikapanda123 <swagatikapanda164@gmail.com>
2020-10-17 03:29:19 +0530

    commiting changes

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
+import {Grid,Cell} from 'react-mdl';
 
 class Projects extends Component{
     render(){
         return(
-            <h1>Projects</h1>
+            <Grid>
+                <Cell col={1}></Cell>
+                <Cell col={2}>
+                    <h4>Featured Projects</h4>
+                </Cell>
+                <Cell col={6}>
+                    <img src="images/project1.png" style={{width:'650px', heigh:'350px'}} alt="project"></img>
+                </Cell>
+                <Cell col={2}><h5>StarSocial</h5>
+                <p>This is a simple Social Clone Application built using Python and DJango Framework.</p>
+                </Cell>
+            </Grid>
         );
     }
 }
